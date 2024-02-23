dc6d3ece46e1bc417e10d64f0d113bb3596338de
Pradeep <pradeepkumarhe1989@gmail.com>
2024-02-23 14:37:50 +0530

    updated registration fields

--- src/components/Registration/register.js
+++ src/components/Registration/register.js
@@ -1,34 +1,80 @@
-import React from 'react'
-import './register.css'
-const Register = ({setSwitchtoComp}) => {
+import React from "react";
+import "./register.css";
+const Register = ({ setSwitchtoComp }) => {
   return (
-    <div className='regWrapper'>
-        <div className='title'>Register</div>
-        <div className='inputFields'>
-           <div className='inputField'>
-           <input placeholder='FirstName'  />
-           </div>
-            <div className='inputField'>
-            <input placeholder='LastName' />
-            </div>
-            <div className='inputField'>
-            <input placeholder='Email'  />
-            </div>
-           <div className='inputField'>
-           <input placeholder='Password' />
-           </div>
-           <div className='inputField'>
-           <input placeholder='Confirm Password' />
-           </div>
+    <div className="regWrapper">
+      <div className="title">Register</div>
+      <div className="inputFields">
+        <div className="inputField">
+          <input placeholder="FirstName" />
         </div>
-        <div className='actionBtn'>
-            <button className='btn'>Register</button>
+        <div className="inputField">
+          <input placeholder="LastName" />
         </div>
-        <div className='text'>
-           <span className='link' onClick={()=>setSwitchtoComp("login")}>Login</span> instead??
+        <div className="inputField">
+          <input placeholder="Email" />
         </div>
-    </div>
-  )
-}
 
-export default Register
+        <div className="inputField">
+          <input placeholder="Country" />
+        </div>
+        <div className="inputField">
+          <input placeholder="Phone number" />
+        </div>
+
+        <div className="inputField">
+          <input placeholder="Company Name" />
+        </div>
+        <div className="inputField">
+          <input placeholder="Company Type" />
+        </div>
+
+        <div className="inputField">
+          <input placeholder="Website" />
+        </div>
+        <div className="inputField">
+          <input placeholder="Number of employees" />
+        </div>
+
+        <div className="inputField">
+          <input placeholder="Country" />
+        </div>
+        <div className="inputField">
+          <input placeholder="State" />
+        </div>
+        <div className="inputField">
+          <input placeholder="City" />
+        </div>
+        <div className="inputField">
+          <input placeholder="Zipcode" />
+        </div>
+        <div className="inputField">
+          <input placeholder="Street" />
+        </div>
+        
+
+        <div className="inputField">
+          <input placeholder="Country" />
+        </div>
+        <div className="inputField">
+          <input placeholder="Phone" />
+        </div>
+        <div className="inputField">
+          <input placeholder="Extension" />
+        </div>
+
+      </div>
+      <div className="actionBtn">
+        <button className="btn">Register</button>
+      </div>
+      <div className="text">
+        <span className="link" onClick={() => setSwitchtoComp("login")}>
+          Login
+        </span>{" "}
+        instead??
+      </div>
+    </div>
+  );
+};
+
+export default Register;
